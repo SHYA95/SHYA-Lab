a17cb6011015bcd47e21009b8d9f33cd348b5214
SHYA95 <yassershrouk254@gmail.com>
2025-10-07 23:16:22 +0300

    Add : Xcode project file

---
+++ ShyaLab/ShyaLab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShyaLab
+//
+//  Created by Shrouk Yasser on 07/10/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ShyaLab/ShyaLabTests/ShyaLabTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShyaLabTests.swift
+//  ShyaLabTests
+//
+//  Created by Shrouk Yasser on 07/10/2025.
+//
+
+import Testing
+@testable import ShyaLab
+
+struct ShyaLabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
