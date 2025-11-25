e32f4a9116da3ada56a5286d7c2e8c065f45defe
SHYA95 <yassershrouk254@gmail.com>
2025-11-25 18:31:50 +0200

    adding the logic of the viewmodel to the main screen

--- ShyaLab/MovieApp/Features/MoviesHome/ViewModel/HomeViewModel.swift
+++ ShyaLab/MovieApp/Features/MoviesHome/ViewModel/HomeViewModel.swift
@@ -8,44 +8,74 @@
 import Foundation
 import Combine
 
+// MARK: - Protocol
 protocol MoviesHomeViewModelProtocol: AnyObject {
-    var state: MoviesHomeViewState { get }
-    var statePublisher: AnyPublisher<MoviesHomeViewState, Never> { get }
+    // Outputs
     var title: String { get }
+    var isLoadingPublisher: AnyPublisher<Bool, Never> { get }
+    var moviesPublisher: AnyPublisher<[Movie], Never> { get }
+    var errorPublisher: AnyPublisher<String, Never> { get }
+    
+    // Inputs
     func loadMovies()
 }
 
+// MARK: - ViewModel Implementation
 class MoviesHomeViewModel: MoviesHomeViewModelProtocol {
     
-    // MARK: - Properties
-    @Published var state: MoviesHomeViewState = .idle
-    
+    // MARK: - Private Properties
+    @Published private var state: MoviesHomeViewState = .idle
     private let useCase: FetchMoviesUseCase
     private let screenTitle: String
     
-    // MARK: - Outputs
-    var statePublisher: AnyPublisher<MoviesHomeViewState, Never> {
-        $state.eraseToAnyPublisher()
-    }
-    
-    var title: String {
-        screenTitle
-    }
-    
     // MARK: - Init
-    // We inject the UseCase directly. The VM doesn't know about Repositories anymore.
     init(title: String, useCase: FetchMoviesUseCase) {
         self.screenTitle = title
         self.useCase = useCase
     }
     
+    // MARK: - Outputs (Logic Moved Here)
+    
+    var title: String {
+        return screenTitle
+    }
+    var isLoadingPublisher: AnyPublisher<Bool, Never> {
+        $state
+            .map { state in
+                if case .loading = state { return true }
+                return false
+            }
+            .removeDuplicates()
+            .eraseToAnyPublisher()
+    }
+    
+    var moviesPublisher: AnyPublisher<[Movie], Never> {
+        $state
+            .compactMap { state -> [Movie]? in
+                if case .success(let movies) = state { return movies }
+                if case .empty = state { return [] }
+                return nil
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    var errorPublisher: AnyPublisher<String, Never> {
+        $state
+            .compactMap { state -> String? in
+                if case .failure(let message) = state { return message }
+                return nil
+            }
+            .eraseToAnyPublisher()
+    }
+    
     // MARK: - Inputs
+    
     func loadMovies() {
         state = .loading
-        
+      
         Task { @MainActor in
             do {
-               let movies = try await useCase.execute()
+                let movies = try await useCase.execute()
                 state = movies.isEmpty ? .empty : .success(movies)
                 
             } catch let error as AppErrorType {
