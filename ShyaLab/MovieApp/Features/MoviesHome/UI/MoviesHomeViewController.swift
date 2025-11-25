e7f406d2ce37b319c26a57790e19a51b2bb03e7f
SHYA95 <yassershrouk254@gmail.com>
2025-11-25 12:07:17 +0200

    Wip: home screen

--- ShyaLab/MovieApp/Features/MoviesHome/UI/MoviesHomeViewController.swift
+++ ShyaLab/MovieApp/Features/MoviesHome/UI/MoviesHomeViewController.swift
@@ -6,24 +6,42 @@
 //
 
 import UIKit
+import Combine
 
 class MoviesHomeViewController: UIViewController {
+    
+    //MARK: Outlets
+    @IBOutlet weak var tableview: UITableView!
+    
+    // MARK: - Properties
+    private let viewModel: MoviesHomeViewModelProtocol
+    private var cancellables = Set<AnyCancellable>()
 
+    // MARK: - Init
+    
+    init(viewModel: MoviesHomeViewModelProtocol) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupUI()
+        bindViewModel()
+        viewModel.loadMovies()
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    private func setupUI() {
+        view.backgroundColor = .systemBackground
     }
-    */
-
+    
+    private func bindViewModel() {
+        // Binding logic will go here
+    }
 }
